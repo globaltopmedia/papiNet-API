cac337d251d6be3a09da5b3c03c7208876d1250c
Patrice Krakow <patrice.krakow@gmail.com>
2020-08-12 16:06:47 +0200

    Add "Stora Enso | Kvarnsveden Mill" in the mock

--- 0.1.0/mock/app.js
+++ 0.1.0/mock/app.js
@@ -26,6 +26,17 @@ const location2 = {
   "city": "Epinal",
   "country": "FR"
 };
+const location3 = {
+  "id": "961d7d05-8bf0-45e0-8e20-1fc7ed462c7c",
+  "iana": "1.3.6.1.4.1.19270.424",
+  "name": "Stora Enso Paper AB - Kvarnsveden Mill",
+  "address": [
+    "Kvarnsvedsvägen 420"
+  ],
+  "postalCode": "784 66",
+  "city": "Borlänge",
+  "country": "SE"
+};
 
 //
 // GET /locations?<field>=...
@@ -54,11 +65,21 @@ app.get('/locations', (req, res) => {
       location1
     ];
 
-  } else {
+  } else if (req.query.iana == location3.iana ||
+      req.query.name        == location3.name ||
+      req.query.postalCode  == location3.postalCode ||
+      req.query.city        == location3.city ||
+      req.query.country     == location3.country) {
 
+  response = [
+    location3
+  ];
+
+  } else {
     response = [
       location1,
-      location2
+      location2,
+      location3
     ];
 
   }
@@ -94,7 +115,28 @@ app.get('/locations/5b7c65f5-ce05-4fe9-8f71-aab9a094271c/opening-hours', (req, r
   });
 });
 
-app.get('/locations/"5ad7ef7d-1c05-4f10-a7c8-4165f7209339/opening-hours', (req, res) => {
+app.get('/locations/5ad7ef7d-1c05-4f10-a7c8-4165f7209339/opening-hours', (req, res) => {
+  res.send({
+    "tuesday": [
+      {
+        "open": "09:00",
+        "close": "12:00"
+      },
+      {
+        "open": "13:00",
+        "close": "20:00"
+      }
+    ],
+    "wednesday": [
+      {
+        "open": "00:00",
+        "close": "23:00"
+      }
+    ]
+  });
+});
+
+app.get('/locations/961d7d05-8bf0-45e0-8e20-1fc7ed462c7c/opening-hours', (req, res) => {
   res.send({
     "tuesday": [
       {
